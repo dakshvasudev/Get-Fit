2d60b900e16c192f6de4ec8507fb392c4673e212
dakshvasudev <91746245+dakshvasudev@users.noreply.github.com>
2024-06-21 13:49:23 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Hero from "./components/Hero";
+import Generator from "./components/Generator";
+import Workout from "./components/Workout";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Hero />
+      <Generator />
+      <Workout />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Workout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Workout = () => {
+  return <div className="">Workout</div>;
+};
+
+export default Workout;
